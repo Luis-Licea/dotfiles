3ead20047375b6d8bfb1b4d48d5b23bff55de4dc
Luis David Licea Torres <luislicea1011@hotmail.com>
2021-09-19 02:36:30 -0500

    Delete xorg.conf.d

--- etc/X11/xorg.conf.d
+++
@@ -1,7 +0,0 @@
-# Define touchpad behaviour for DWM.
-Section "InputClass"
-    Identifier "touchpad catchall"
-    Driver "libinput"
-    Option "Tapping" "on"
-    Option "NaturalScrolling" "true"
-EndSection
