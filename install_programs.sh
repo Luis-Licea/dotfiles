7092a94ba3932cd57c13e9b8b23275763bcfb2b4
Luis David Licea Torres <luislicea@hotmail.com>
2022-06-04 18:17:32 -0600

    Keep track of tested and liked programs.

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+# NOTE: Use 'locate' command more often! It is similar to fzf.
 # TODO: Backup this file, VSCode settings.json, keybindings.json, .zshrc for oh-my-zsh, dolvim settings.
 
 # Install using pacman.
@@ -25,6 +26,8 @@ pacman_programs=(
     # zathura             # Minimalistic document viewer.
     # evince              # GTK PDF Viewer.
     # lollypop            # Music player (no support for star ratings).
+    # pdfarranger         # Merge, split, rotate, crop and rearrange PDFs.
+    # gscan2pdf           # PDF tool. Can remove background from scanned PDFs.
     audacium            # Free version of Audacity.
     cmus                # TUI music player.
     deja-dup            # File backups.
@@ -41,6 +44,10 @@ pacman_programs=(
     thunderbird         # Email client.
     yt-dlp              # ty-dlp fork with added features and patches.
     most                # A terminal pager similar to 'more' and 'less'. NOTE: Set PAGER=most in .zshrc.
+
+    # Terminal file manager and viewer.
+    ranger    # Simple, vim-like file manager.
+    ueberzug  # Display images in X11 in ranger. NOTE: Configure rc.conf.
 
     # System
     p7zip               # Command line archiver with high compression ratio.
@@ -107,10 +114,16 @@ aur_programs=(
 )
 
 modern_unix=(
-    # Install with pacman.
+    # Intalled and liked.
     bat       # A cat clone with syntax highlighting and Git integration.
     # exa       # A modern replacement for ls. (lsd is prettier by default)
     lsd       # The next gen file listing command. Backwards compatible with ls.
+    dog       # A user-friendly command-line DNS client (dig on steroids).
+    fzf       # A general purpose command-line fuzzy finder.
+    cheat     # Create and view interactive cheatsheets on the command-line.
+    tldr      # A community effort to simplify man pages with practical examples.
+
+    # Not xperimened with.
     git-delta # A viewer for git and diff output
     dust      # A more intuitive version of du written in rust.
     duf       # A better df alternative.
@@ -118,16 +131,10 @@ modern_unix=(
     fd        # A simple, fast and user-friendly alternative to find.
     ripgrep   # An extremely fast alternative to grep that respects your gitignore.
     ag        # A code searching tool similar to ack, but faster.
-    fzf       # A general purpose command-line fuzzy finder.
     mcfly     # Fly through your shell history.
     choose    # A human-friendly and fast alternative to cut and sometimes awk.
     jq        # sed for JSON data.
     sd        # An intuitive find & replace CLI (sed alternative).
-    cheat     # Create and view interactive cheatsheets on the command-line.
-    tldr      # A community effort to simplify man pages with practical examples.
-    bottom    # Yet another cross-platform graphical process/system monitor.
-    glances   # Glances an Eye on your system (top/htop alternative).
-    gtop      # System monitoring dashboard for terminal.
     hyperfine # A command-line benchmarking tool.
     gping     # ping, but with a graph.
     procs     # A modern replacement for ps written in Rust.
@@ -135,9 +142,7 @@ modern_unix=(
     curlie    # The power of curl, the ease of use of httpie.
     xh        # A friendly and fast tool for sending HTTP requests. It reimplements as much as possible of HTTPie's excellent design, with a focus on improved performance.
     zoxide    # A smarter cd command inspired by z.
-    dog       # A user-friendly command-line DNS client (dig on steroids).
 )
-
 
 # Install using plasma-discover.
 flatpak_programs=(
