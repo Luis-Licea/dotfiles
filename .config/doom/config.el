00fa87ca9587475d805a2de74ff1c742ed098b59
Luis David Licea Torres <luislicea@hotmail.com>
2023-02-19 16:00:13 -0600

    Doom Emacs: Add name. Auto-add task timestamps.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Luis David Licea Torres"
+      user-mail-address "luisliceatorres@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -41,6 +41,9 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+
+;; Automatically add time stamp to finished to-do tasks.
+(setq org-log-done 'time)
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
