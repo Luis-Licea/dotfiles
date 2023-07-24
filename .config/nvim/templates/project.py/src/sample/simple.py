46b84e49af937f114e798499b2742bd3ed87b9ee
Luis David Licea Torres <luislicea@hotmail.com>
2023-07-23 21:01:34 -0500

    Nvim: Improve Python sample project.

--- .config/nvim/templates/project.py/src/sample/simple.py
+++ .config/nvim/templates/project.py/src/sample/simple.py
@@ -1,2 +1,14 @@
-def add_one(number):
+def add_one(number: int) -> int:
+    """Increments a number.
+
+    Example:
+    >>> add_one(10)
+    10
+
+    Args:
+        number (int): The number to increment.
+
+    Returns:
+        int: The incremented number.
+    """
     return number + 1
--- .config/nvim/templates/project.py/tests/test_simple.py
+++ .config/nvim/templates/project.py/tests/test_simple.py
@@ -2,16 +2,26 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
-import unittest
+from doctest import testmod
+from unittest import TestCase, main
+from sample import simple
 
 from sample.simple import add_one
 
 
-class TestSimple(unittest.TestCase):
+class TestSimple(TestCase):
 
     def test_add_one(self):
         self.assertEqual(add_one(5), 6)
 
+    def test_examples(self):
+        """This test wont work properly until the package has been installed
+        into the virtual environment."""
+        testResults = testmod(simple)
+        self.assertNotEqual(testResults.attempted, 0)
+        self.assertEqual(testResults.failed, 0)
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
