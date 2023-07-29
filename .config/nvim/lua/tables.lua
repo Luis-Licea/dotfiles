04ae273740f69c5f7236f286ad09eb8fa65b74cf
Luis David Licea Torres <luislicea@hotmail.com>
2023-07-29 10:39:48 -0500

    Nvim: Add tables compare method.

--- .config/nvim/lua/tables.lua
+++ .config/nvim/lua/tables.lua
@@ -4,7 +4,7 @@
 
 local tables = {}
 
---- Join two dictionaries.
+---Join two dictionaries.
 ---@param table1 table The table that will be modified.
 ---@param table2 table The table to append.
 function tables.append(table1, table2)
@@ -15,9 +15,39 @@ function tables.append(table1, table2)
     end
 end
 
+---Return whether two arrays are the same.
+---```lua
+---local array = { { 'This', 'is' }, { 'an' }, 'example' }
+---compare(array, { { 'This', 'is' }, { 'an' }, 'example' }, 'The arrays are the same.'))
+---```
+---
+---
+---@param array1 Array The first array to compare.
+---@param array2 Array The second array to compare.
+---@return boolean
+function tables.compare(array1, array2)
+    for i, v in pairs(array1) do
+        if type(v) == 'table' then
+            if tables.compare(array2[i], v) == false then
+                return false
+            end
+        else
+            if v ~= array2[i] then
+                return false
+            end
+        end
+    end
+    return true
+end
 
---- Join two or more arrays and return the new array.
----@vararg Array|number|string the arrays to merge.
+---Join two or more arrays or strings and return the new array.
+---```lua
+---local array = tables.merge({"This", "is"}, {"an"}, "example")
+-- assert(tables.compare(array, { 'This', 'is', 'an', 'example' }), 'The arrays are the same.')
+---```
+---
+---
+---@vararg Array|number|string the arrays or strings to merge.
 ---@return Array the new array formed from all the passed arrays.
 function tables.merge(...)
     local result = {}
