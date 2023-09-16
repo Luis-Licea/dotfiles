329f2cdfdb3873f72acbd1c241d73e8651fb4b76
Luis David Licea Torres <luislicea@hotmail.com>
2023-09-16 10:35:31 -0500

    Nvim: Disable IFS in bash scratchpads.

--- .config/nvim/templates/skeleton.bash
+++ .config/nvim/templates/skeleton.bash
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 # Exit on errors, undefined variables, and unmask pipeline errors.
 set -euo pipefail
-# Internal Field Separator: Split new-lines and tabs, not spaces.
-IFS=$'\n\t'
+# No Internal Field Separator. To split spaces, newlines, and tabs: IFS=$' \n\t'
+unset IFS
+
+#######################################
+# Split a string using the given character, and store the array in the name-ref.
+# Arguments:
+#   1: The string to split.
+#   2: The separator character, like ' ', $'\n', or $'\t'.
+#   3: The variable name-ref.
+# Example:
+#   $ split $'hello\nworld' $'\n' words
+#   $ declare -p words
+#   declare -a words=([0]="hello" [1]="world")
+#######################################
+split() {
+	declare -nr array="$3"
+	local IFS="$2"
+	array=($1)
+}
