cd4875981772b418948c7ab9cf35e81cd022dce2
Luis David Licea Torres <luislicea@hotmail.com>
2023-09-16 10:47:15 -0500

    Nvim: Add random choice picker to bash scratchpad.

--- .config/nvim/templates/skeleton.bash
+++ .config/nvim/templates/skeleton.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Exit on errors, undefined variables, and unmask pipeline errors.
 set -euo pipefail
 # No Internal Field Separator. To split spaces, newlines, and tabs: IFS=$' \n\t'
@@ -16,7 +17,25 @@ unset IFS
 #   declare -a words=([0]="hello" [1]="world")
 #######################################
 split() {
-	declare -nr array="$3"
-	local IFS="$2"
-	array=($1)
+    declare -nr array="$3"
+    local IFS="$2"
+    array=($1)
+}
+
+#######################################
+# Selects a random element from the given list.
+# Arguments:
+#   1: The list name-ref.
+# Standard output:
+#   1: A random element from the list.
+# Example:
+#   $ list=("choice 1", "choice 2")
+#   $ choice list
+#   choice 2
+#######################################
+choice() {
+    declare -nr arguments=$1
+    readonly count=${#arguments[@]}
+    readonly choice=${arguments[RANDOM%${count}]}
+    printf "%s" "$choice"
 }
