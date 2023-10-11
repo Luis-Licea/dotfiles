839a249721b7c956fd047d6ddc97d4be6741a980
Luis David Licea Torres <luislicea@hotmail.com>
2023-10-10 21:31:06 -0500

    Nvim: Freeze indent-blankline plugin.

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,8 @@
 -- Show indentation lines.
 return {
     'lukas-reineke/indent-blankline.nvim',
+    commit = "4541d690816cb99a7fc248f1486aa87f3abce91c",
+    branch = "master",
     config = function()
         require("indent_blankline").setup({
             show_current_context = true,
