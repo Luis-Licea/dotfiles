f7682484278ff4075e80ef40b6fab45079b49b2b
Luis David Licea Torres <luislicea@hotmail.com>
2024-06-08 16:27:01 -0500

    Neovim: Fix toggle comment keybinding.

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -5,12 +5,12 @@ return {
         require('Comment').setup({
             -- LHS of toggle mappings in NORMAL mode.
             toggler = {
-                line = '<C-_>', -- Line-comment toggle keymap.
+                line = '<C-/>', -- Line-comment toggle keymap.
                 block = 'gbc', -- Block-comment toggle keymap.
             },
             -- LHS of operator-pending mappings in NORMAL and VISUAL mode.
             opleader = {
-                line = '<C-_>', -- Line-comment keymap.
+                line = '<C-/>', -- Line-comment keymap.
                 block = 'gb', -- Block-comment keymap.
             },
             -- Enable keybindings: `false` to not create mappings
