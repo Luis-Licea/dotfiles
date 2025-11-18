60320a2bbae7ad45a1004973f7563e9be0ff126d
Luis David Licea Torres <luislicea@hotmail.com>
2025-11-18 00:38:51 -0600

    Add Reaper audio config

--- .config/home-manager/nixos/audio.nix
+++ .config/home-manager/nixos/audio.nix
@@ -1,29 +1,145 @@
-{...}: {
-  # Enable sound with pipewire.
-  security.rtkit.enable = true; # Optional but recommended.
+{
+  pkgs,
+  ...
+}: {
+  # Load the sequencer and midi kernel modules.
+  boot.kernelModules = ["snd-seq" "snd-rawmidi"];
+  users.extraUsers.luis.extraGroups = ["audio" "realtime"];
 
-  # Audio settings.
   services.pipewire = {
-    alsa.enable = true;
     enable = true;
-    # jack.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
     pulse.enable = true;
-    wireplumber.enable = true;
   };
 
+  # Must disable pulseaudio to allow for the pipewire pulseaudio emulation.
+  services.pulseaudio.enable = false;
+
   # Needed for Zrythm.
-  # users.extraUsers.luis.extraGroups = ["jackaudio"];
-  # services.jack = {
-  #   jackd.enable = true;
-  #   # support ALSA only programs via ALSA JACK PCM plugin
-  #   alsa.enable = false;
-  #   # support ALSA only programs via loopback device (supports programs like Steam)
-  #   loopback = {
-  #     enable = true;
-  #     # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-  #     #dmixConfig = ''
-  #     #  period_size 2048
-  #     #'';
-  #   };
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "95";
+    }
+    {
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+  ];
+
+  # # Allow detection of VST plugins like Helm.
+  # environment.variables = let
+  #   makePluginPath = format:
+  #     (lib.makeSearchPath format [
+  #       "$HOME/.nix-profile/lib"
+  #       "$HOME/.local/state/nix/profile/lib"
+  #       "/run/current-system/sw/lib"
+  #       "/etc/profiles/per-user/$USER/lib"
+  #     ])
+  #     + ":$HOME/.${format}";
+  # in {
+  #   DSSI_PATH = makePluginPath "dssi";
+  #   LADSPA_PATH = makePluginPath "ladspa";
+  #   LV2_PATH = makePluginPath "lv2";
+  #   LXVST_PATH = makePluginPath "lxvst";
+  #   VST_PATH = makePluginPath "vst";
+  #   VST3_PATH = makePluginPath "vst3";
   # };
+
+  environment.systemPackages = with pkgs; [
+    reaper
+    reaper-reapack-extension
+
+    # ardour
+    # zrythm
+
+    # carla
+    # calf
+    geonkick
+    helm
+    lsp-plugins
+    # noise-repellent
+    # paulstretch
+    # sfizz
+    # surge
+    # swh_lv2
+    vmpk
+    x42-avldrums
+    # x42-gmsynth
+    # x42-plugins
+    zam-plugins
+    # zynaddsubfx # Plugin
+
+    # ffado
+    # alsa-utils
+    # pkgs-brian.ardour
+    # spotify
+    # alsa-lib
+    # libjack2
+    # jack2
+    # qjackctl
+    # pavucontrol
+    # jack_capture
+    # pulseaudioFull
+    # audacious
+    # qpwgraph
+    # show-midi
+    # mopidy
+    # mopidy-bandcamp
+    # mopidy-iris
+    # mopidy-notify
+    # mopidy-spotify
+    # mopidy-soundcloud
+    # lmms
+    # lsp-plugins
+  ];
 }
+# hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+# # Enable sound with pipewire.
+# security.rtkit.enable = true; # Optional but recommended.
+#
+# # Audio settings.
+# services.pipewire = {
+#   alsa.enable = true;
+#   enable = true;
+#   # jack.enable = true;
+#   pulse.enable = true;
+#   wireplumber.enable = true;
+# };
+# services.jack = {
+#   jackd.enable = true;
+#   # support ALSA only programs via ALSA JACK PCM plugin
+#   alsa.enable = false;
+#   # support ALSA only programs via loopback device (supports programs like Steam)
+#   loopback = {
+#     enable = true;
+#     # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
+#     #dmixConfig = ''
+#     #  period_size 2048
+#     #'';
+#   };
+# };
+# users.extraUsers.luis.extraGroups = [ "jackaudio" ];
+# Needed for Zrythm.
+# users.extraUsers.luis.extraGroups = ["jackaudio"];
+# services.jack = {
+#   jackd.enable = true;
+#   # support ALSA only programs via ALSA JACK PCM plugin
+#   alsa.enable = false;
+#   # support ALSA only programs via loopback device (supports programs like Steam)
+#   loopback = {
+#     enable = true;
+#     # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
+#     #dmixConfig = ''
+#     #  period_size 2048
+#     #'';
+#   };
+# };
