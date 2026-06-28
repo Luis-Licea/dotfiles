f42aa4ff01792c4360ce0d2fa30be4a9b3aa86f1
Luis David Licea Torres <luislicea@hotmail.com>
2026-06-28 12:05:46 -0500

    Delete dwl

--- .config/dwl/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export XDG_SESSION_TYPE=wayland
-export XDG_CURRENT_DESKTOP=dwl
-
-# Run emacs deamon so that clients start up quickly.
-emacs --daemon
-# Redirect tag information into file.
-dwl > "$HOME/.cache/dwltags"
